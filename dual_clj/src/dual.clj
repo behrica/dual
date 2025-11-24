85c3edc5fcb22f70aa2dc5df45945f59f54d95d5
Carsten Behring <carsten.behring@gmail.com>
2025-11-24 23:14:06 +0100

    cleanup

--- dual_clj/src/dual.clj
+++ dual_clj/src/dual.clj
@@ -1,8 +1,6 @@
 (ns dual
   (:require [clj-http.client :as client]))
 
-
-
 (->>
  (client/get "http://dual-basilisp:8000/text-to-doc"
              {:query-params {:text "Barack Obama was born in Hawaii. He was elected president in 2008."}
